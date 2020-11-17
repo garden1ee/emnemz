4e71a6fa20dbdc7c89a666ecd068bba58cff9b69
garden1ee <garden1ee@users.noreply.github.com>
2020-11-18 01:45:57 +0900

    bootstrap 어떻게쓰는거지

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Something from './Something';
 
 function App() {
