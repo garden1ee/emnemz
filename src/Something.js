e1bae3ba25b95f4e2a8099df076734ce7ef30055
garden1ee <garden1ee@users.noreply.github.com>
2020-11-18 01:22:29 +0900

    'bootstrap?'

---
+++ src/Something.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Something() {
+    return <div>
+        <button className="btn btn-default">Bootstrap test</button>
+    </div>
+}
+
+export default Something;
