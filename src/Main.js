0a6fd1787d0d799f89d2cf8842c0788d9c08bd2d
Hyunwoo <zkdlgusdn98@gmail.com>
2020-11-18 17:48:56 +0900

    header

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const btnstyle={
   border: 0,
   color: "black",
@@ -30,19 +29,15 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main header">
-                  <button style={btnstyle} className="header">Service Name</button>
-                  <button style={stnstyle} className="header">Writing Room</button>
-                  <button style={stnstyle} className="header">Finished Works</button>
-                  <button style={stnstyle} className="header">My Page</button>
-                  <button style={stnstyle}  className="header">Setting</button>
-                  <button style={stnstyle} className="header">Logout</button>
                   <hr></hr>
                   <input style={searchbarstyle}/>
-                  <button style={stnstyle} classname="hashtag">hastag </button>
                   <button onClick={()=>{alert("조금더 시간을 주시면 구현됩니다")}}> 검색</button>
             </div>
+
           )
 
     }
 }
+
+
 export default Main;
