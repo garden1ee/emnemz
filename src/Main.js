21c74c5e17f27dff2c58ad38728280e02c6880ad
zee2013234 <zee2013234@kaist.ac.kr>
2020-11-19 06:33:05 +0900

    Removed Const and put it in style.css
    Removed the const styling formats and reflected the changes in style.css

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,6 @@
 import React from 'react';
 import PageHeader from './PageHeader.js';
 
-const btnstyle={
-  border: 0,
-  color: "black",
-  background: "white",
-  fontSize: 36,
-  marginLeft: 30,
-  marginTop: 10,
-}
-
-const stnstyle={
-  border: 0,
-  color: "black",
-  background: "white",
-  fontSize: 24,
-  marginLeft:30,
-}
-const searchbarstyle={
-  color: "black",
-  background: "#C4C4C4",
-  fontSize: 17,
-  marginLeft:30,
-  height: 29,
-  width: 600,
-  marginRight: 10,
-  marginTop: 10,  
-}
 
 class Main extends React.Component {
     render() {
