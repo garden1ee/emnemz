6e0be33d115e114d7c347ca69bb3ce1fda0b9cf8
Hyunwoo <zkdlgusdn98@gmail.com>
2020-11-18 20:34:27 +0900

    searchbar

--- src/Main.js
+++ src/Main.js
@@ -29,9 +29,6 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main header">
-                  <hr></hr>
-                  <input style={searchbarstyle}/>
-                  <button onClick={()=>{alert("조금더 시간을 주시면 구현됩니다")}}> 검색</button>
             </div>
 
           )
