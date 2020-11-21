4f7c027905efeb426456f8eae52f938448b9a9b8
garden1ee <garden1ee@users.noreply.github.com>
2020-11-21 12:51:33 +0900

    basic styling

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -3,40 +3,23 @@ import { Link, Route } from 'react-router-dom';
 import { RoomList, PublishedList, ProfilePage, MyPage } from 'Pages/Mainpages';
 import { auth } from "../firebase";
 import { withTheme } from '@material-ui/core';
-
-
-const btnstyle1={
-  display:'flex',
-  backgroundColor: 'white'
-}
-
-const btnstyle={
-  border: 0,
-  fontSize: 36,
-
-}
-
-const stnstyle={
-  border: 0,
-  fontSize: 24,
-  marginLeft:30,
-}
+import '../style/Header.css';
 
 const Header = () => {
   return (
     <div>
-      <div style={btnstyle1} className="main header">
-        <nav style={btnstyle1} className="navbar">
-          <button style={btnstyle} className="header"><Link to="/list">롤링롤링</Link></button>
-          <button style={stnstyle} className="header"><Link to="/list">소설 작성방</Link></button>
-          <button style={stnstyle} className="header"><Link to="/library">완결 작품들</Link></button>
-          <button style={stnstyle} className="header"><Link to="/mypage">마이 페이지</Link></button>
-          <button style={stnstyle} className="header"><Link to="/mypage">알림</Link></button>
-          <button style={stnstyle} className="header"><Link to="/mypage">설정</Link></button>
-          <button style={stnstyle} className="header" onClick = {() => {auth.signOut();}}><Link to="/">로그아웃</Link></button>
-          <hr></hr>
-        </nav>
-      </div>
+      <ul className="header">
+        <li><Link to="/list" className="link logo">롤링롤링</Link></li>
+        <li><Link to="/list" className="link">소설 작성방</Link></li>
+        <li><Link to="/library" className="link la">완결 작품들</Link></li>
+        <div className="rightones">
+        <li><Link to="/mypage" className="link right">마이페이지</Link></li>
+        <li><Link to="/mypage" className="link right">알림</Link></li>
+        <li><Link to="/mypage" className="link right">설정</Link></li>
+        <li onClick = {() => {auth.signOut();}}><Link to="/" className="link right">로그아웃</Link></li>
+        </div>
+      </ul>
+      <hr></hr>
       <Route path="/library" component={PublishedList}/>
       <Route path="/mypage" component={ProfilePage}/>
       <Route path="/list" component={RoomList}/>
