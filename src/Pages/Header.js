d05f986890a6810453ac54e5e15c2ddaa2729be1
garden1ee <garden1ee@users.noreply.github.com>
2020-11-23 09:04:03 +0900

    header activate current state

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -13,14 +13,28 @@ const Header = () => {
 
     const toggle = () => setModal(!modal);
 
+    const [active, setActive] = useState("");
+    const togglea = (event) => {
+      const { id } = event.target;
+      if (active === id) {
+        setActive("");
+      } else {
+        setActive(id);
+        console.log(active);
+      }
+    }
+
   return (
     <div className="HD-header">
       <ul className="HD-navbar">
         <li><Link to="/list" className="Hd-link HD-logo">롤링롤링</Link></li>
-        <li><Link to="/list" className="Hd-link">소설 작성방</Link></li>
-        <li><Link to="/library" className="Hd-link HD-la">완결 작품들</Link></li>
+        <li><Link to="/list" id="Hd-list" className={`Hd-link ${active==="Hd-list"? 'Hd-active':""}`}
+        onClick={togglea}>소설 작성방</Link></li>
+        <li><Link to="/library" id="Hd-pub" className={`Hd-link HD-la ${active==="Hd-pub"? 'Hd-active':""}`}
+        onClick={togglea}>완결 작품들</Link></li>
         <div className="HD-rightones">
-        <li><Link to="/mypage" className="Hd-link">마이페이지</Link></li>
+        <li><Link to="/mypage" id="Hd-My" className={`Hd-link ${active==="Hd-My"? 'Hd-active':""}`}
+        onClick={togglea}>마이페이지</Link></li>
         <li><Link to="/mypage" className="Hd-link">알림</Link></li>
         <li><Link to="/mypage" className="Hd-link">설정</Link></li>
         <li onClick = {() => {auth.signOut();}}><Link to="/" className="Hd-link">로그아웃</Link></li>
--- src/Pages/WritingRoom.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EnterWritingroom from "./EnterWritingroom";
-
-function WritingRoom() {
-  return (
-      <EnterWritingroom />
-  );
-}
-export default WritingRoom;
