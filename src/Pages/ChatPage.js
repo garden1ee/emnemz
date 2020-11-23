df4e3c940eb9942a325c2c25db4ab10bd9c836e0
garden1ee <garden1ee@users.noreply.github.com>
2020-11-24 03:15:56 +0900

    listenerworking

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -1,6 +1,6 @@
 import React, {Component, useState, useEffect } from 'react';
 import { MDBRow, MDBCol, MDBListGroup} from "mdbreact";
-import { getRoomDocument, getScript, scriptonChange } from "../firebase";
+import { getRoomDocument, getScript, firestore } from "../firebase";
 import aron_img from "../Pages/Images_character/aron.png";
 import ruffi_img from "../Pages/Images_character/ruffi.png";
 import nami_img from "../Pages/Images_character/nami.png";
@@ -10,17 +10,19 @@ import ChatMessage from "./ChatMessage";
 
 const ChatPage = (props) => {
     const [scripts, setScripts] = useState([]);
+    const [news, setNews] = useState(true);
     const room_id = props.roomid;
     useEffect(() => {
+      firestore.doc(`rooms/${room_id}`).onSnapshot(setNews(true));
       async function loadScript() {
         const scriptDB = await getScript(room_id);
         const { scripts } = scriptDB;
         setScripts(scripts);
       };
-      loadScript();
-      scriptonChange(room_id).then(
-        loadScript()
-      );
+      if (news==true) {
+        loadScript();
+        setNews(false);
+      }
       
     })
     return (
