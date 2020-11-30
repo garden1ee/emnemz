b336e2c81256dd20281fd6749131f2ddf2be52bf
garden1ee <little0potato@kaist.ac.kr>
2020-12-01 00:55:57 +0900

    editbtn style

--- src/Pages/ChatMessage.js
+++ src/Pages/ChatMessage.js
@@ -15,9 +15,9 @@ function ChatMessage(props) {
     <li className="chat-message">
     <div className="chat-profilebox"><img src={photoURL||'https://i.ibb.co/ChncsT7/1-W35-QUSv-Gpc-Lux-Po3-SRTH4w.png'} alt="character"  style={{width: 50, height: 50, borderRadius: 50/ 2}} />
     <p>{character}</p></div>
-      <div className="chat-scriptbox"> {text} {uid === auth.currentUser.uid ? <button className="btn btn-primary btn-sm" 
-                                                    style={{width:70, height:30, verticalAlign: "center"}}>편집</button>:<div/>}
-      </div>
+      <div className="chat-scriptbox"> {text} 
+      </div> {uid === auth.currentUser.uid ?   <button className="btn btn-primary btn-sm editBtn" 
+                                                    style={{width:50, height:30, verticalAlign: "center"}}>편집</button>:<div/>}
       <small hidden className="pull-right text-muted">
           <i className="far fa-clock"/> {createdAt && createdAt.seconds}
         </small>                                    
@@ -25,8 +25,8 @@ function ChatMessage(props) {
       :
     <li className="chat-message">
       <div>
-       <div className="chat-actionbox">{character}, {text} {uid === auth.currentUser.uid ? <button className="btn btn-primary btn-sm" 
-                                                    style={{width:70, height:30, verticalAlign: "center"}}>편집</button>:<div/>}
+       <div className="chat-actionbox">{character}, {text} {uid === auth.currentUser.uid ? <button className="btn btn-primary btn-sm editBtn" 
+                                                    style={{width:50, height:30, verticalAlign: "center"}}>편집</button>:<div/>}
       </div>
       <small hidden className="pull-right text-muted">
           <i className="far fa-clock"/> {createdAt && createdAt.seconds}
