0d6e9780231d7666fe4c591371ce35d24013e44e
garden1ee <little0potato@kaist.ac.kr>
2020-12-04 00:20:05 +0900

    signup

--- src/Pages/Mainpages/Modals/ChatSignupModal.js
+++ src/Pages/Mainpages/Modals/ChatSignupModal.js
@@ -7,10 +7,37 @@ export class ChatSignupModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            setSubmitIsOpen: false
+            setSubmitIsOpen: false,
+            FstPf: '',
+            SndPf: '',
+            TrdPf: '',
+            reason: '',
+            goal:'',
+            responsible:''
         };
     }
-
+    handleChange(event) {
+        if (event.target.name=="FstPf") this.setState({FstPf: event.target.value});
+        else if (event.target.name=="SndPf") this.setState({SndPf: event.target.value});
+        else if (event.target.name=="TrdPf") this.setState({TrdPf: event.target.value});
+        else if (event.target.name=="reason") this.setState({reason: event.target.value});
+        else if (event.target.name=="goal") this.setState({goal: event.target.value});
+        else if (event.target.name=="responsible") this.setState({responsible: event.target.value});
+    }
+    async handleSubmit() {
+        const roomRef = firestore.doc(`rooms/${this.props.roomid}`);
+        await roomRef.set({
+            applyform : {
+                FstPf: this.state.FstPf,
+                SndPf: this.state.SndPf,
+                TrdPf: this.state.TrdPf,
+                reason: this.state.reason,
+                goal: this.state.goal,
+                responsible: this.state.responsible,
+                uid: this.props.uid
+            }
+        }, {merge: true});
+    }
     render() {
         let roomid = this.props.roomid;
         let uid = this.props.uid;
@@ -34,6 +61,7 @@ export class ChatSignupModal extends Component {
         </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <form onSubmit={this.handleSubmit}>
                     <div className="row" style={{ marginTop: "5px", marginBottom: "15px", marginLeft: "5px"}}>
                         <h5>선택 가능한 캐릭터</h5>
                     </div>
@@ -52,41 +80,47 @@ export class ChatSignupModal extends Component {
                     <div className="col-10">
                         <div className="row">
                             {this.props.characters.map(c =>
-                            <div><img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</div>)}       
+                            <label><input type="radio" name="FstPf" value={c.name} checked={this.state.FstPf[c.name]} onChange={this.handleChange.bind(this)}/>
+                                <img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</label>)}       
                         </div>
                         <div className="row">
                             {this.props.characters.map(c =>
-                            <div><img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</div>)}
-                            <img src="https://icons-for-free.com/iconfiles/png/512/x-1321215629555778185.png" width="50" height="50" className="profile-pic" />선택 없음
+                            <label><input type="radio" name="SndPf" value={c.name} checked={this.state.SndPf[c.name]} onChange={this.handleChange.bind(this)}/>
+                            <img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</label>)}
+                            <label><input type="radio" name="SndPf" value="none" checked={this.state.SndPf['none']} onChange={this.handleChange.bind(this)}/>
+                            <img src="https://icons-for-free.com/iconfiles/png/512/x-1321215629555778185.png" width="50" height="50" className="profile-pic" />선택 없음</label>
                         </div>
                         <div className="row">
-                            {this.props.characters.map(c =>
-                            <div><img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</div>)}
-                            <img src="https://icons-for-free.com/iconfiles/png/512/x-1321215629555778185.png" width="50" height="50" className="profile-pic" />선택 없음
+                        {this.props.characters.map(c =>
+                            <label><input type="radio" name="TrdPf" value={c.name} checked={this.state.TrdPf[c.name]} onChange={this.handleChange.bind(this)}/>
+                            <img src={c.pic} width="50" height="50" className="profile-pic" /> {c.name}</label>)}
+                            <label><input type="radio" name="TrdPf" value="none" checked={this.state.TrdPf['none']} onChange={this.handleChange.bind(this)}/>
+                            <img src="https://icons-for-free.com/iconfiles/png/512/x-1321215629555778185.png" width="50" height="50" className="profile-pic" />선택 없음</label>
                         </div>
                         </div>
                         </div>
                     <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
                         왜 이 소설에 참가하고 싶은가요?
                     </div>
-                    <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
-                        <input type="text" style={{ width: '100%', marginBottom: '10px'}}/>
+                    <div className="row">
+                        <input type="text" name="reason" onChange={this.handleChange.bind(this)} style={{ width: '80%'}}/>
                     </div>
-                    <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
+                    <div className="row">
                         친구와 어떠한 소설을 작성해 보고 싶나요?
                         </div>
-                    <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
-                        <input type="text" style={{ width: '100%', marginBottom: '10px' }}/>
+                    <div className="row">
+                        <input type="text" name="goal" onChange={this.handleChange.bind(this)} style={{ width: '80%' }}/>
                     </div>
-                    <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
+                    <div className="row">
                         소설이 완성될 때까지 책임감 있게 참여하실 건가요?
                     </div>
-                    <div className="row" style={{marginLeft: '30px', marginRight: '30px'}}>
-                        <input type="text" style={{ width: '100%', marginBottom: '10px' }}/>
+                    <div className="row">
+                        <input type="text" name="responsible" onChange={this.handleChange.bind(this)} style={{ width: '80%' }}/>
                     </div>
+                  </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={() => this.setState({ setSubmitIsOpen: true })} className="primary-btn">등록</Button>
+                    <Button type="submit" onClick={() => this.setState({ setSubmitIsOpen: true })} className="primary-btn">등록</Button>
                     <SubmissionModal submit={addUser} show={this.state.setSubmitIsOpen} onComplete={setSubmitClose} />
                     <Button onClick={this.props.onSubmit} className="secondary-btn">닫기</Button>
                 </Modal.Footer>
